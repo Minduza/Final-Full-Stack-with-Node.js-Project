3661520dcad53d37d54e694fd481917ab96a96f8
Mindaugas Purvis <mindaugas.purvis@gmail.com>
2023-07-04 19:50:09 +0300

    Added GET POST for posts and comments

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Get and Post user
+// Get and Post Users
 
 app.get('/users', async (req, res) => {
   try {
@@ -29,12 +29,90 @@ app.get('/users', async (req, res) => {
 
 app.post('/users', async (req, res) => {
   try {
-    const { name, surname, nickname, birthDate, email, password } = req.body;
+    const user = req.body;
+    const con = await client.connect();
+    const data = await con.db(dbName).collection('users').insertOne(user);
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Get and Post Questions/Posts
+
+app.get('/posts', async (req, res) => {
+  try {
     const con = await client.connect();
     const data = await con
       .db(dbName)
-      .collection('users')
-      .insertOne({ name, surname, nickname, birthDate, email, password });
+      .collection('posts')
+      .aggregate([
+        {
+          $lookup: {
+            from: 'comments', // kitos kolekcijos pavadinimas
+            localField: '_id', // owners kolekcijos raktas per kurį susijungia
+            foreignField: 'postId', // kitos kolekcijos raktas per kurį susijungia
+            as: 'comments', // naujo rakto pavadinimas
+          },
+        },
+      ])
+      .toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post('/posts', async (req, res) => {
+  try {
+    const { dateCreated, text, edited, userId } = req.body;
+    const con = await client.connect();
+    const data = await con
+      .db(dbName)
+      .collection('posts')
+      .insertOne({
+        dateCreated,
+        text,
+        edited,
+        usersId: new ObjectId(userId),
+      });
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Get and Post Answers/Comments
+
+app.get('/posts/:id/answers', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db(dbName).collection('comments').find().toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post('/posts/:id/answers', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { dateCreated, text, edited, usersId } = req.body;
+    const con = await client.connect();
+    const data = await con
+      .db(dbName)
+      .collection('comments')
+      .insertOne({
+        dateCreated,
+        text,
+        edited,
+        usersId,
+        postId: new ObjectId(id),
+      });
     await con.close();
     res.send(data);
   } catch (error) {
