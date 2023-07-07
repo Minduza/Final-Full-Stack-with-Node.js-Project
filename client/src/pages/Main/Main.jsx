e180a55811570992837b819e8e019bbdc9539ee1
Mindaugas Purvis <mindaugas.purvis@gmail.com>
2023-07-07 23:37:47 +0300

    Added posts to Main page.

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
+import PostCard from "../../components/PostCard/PostCard";
+import RegisterLayout from "../../layout/RegisterLayout/RegisterLayout";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const Main = () => {
-  return <div>Main</div>;
+  const [posts, setPosts] = useState([]);
+
+  const navigate = useNavigate();
+
+  axios
+    .get("http://localhost:3000/posts")
+    .then((resp) => resp.data)
+    .then((response) => {
+      setPosts(response);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  const onclickHandler = (id) => navigate(`/posts/${id}`);
+
+  return (
+    <RegisterLayout>
+      {posts.map((post, index) => (
+        <PostCard
+          key={index}
+          onClick={() => onclickHandler(post._id)}
+          nickname={post.nickname}
+          dateCreated={post.dateCreated}
+          title={post.title}
+          text={post.text}
+          commentNumber={post.comments.length}
+          edited={post.edited}
+        />
+      ))}
+    </RegisterLayout>
+  );
 };
 
 export default Main;
