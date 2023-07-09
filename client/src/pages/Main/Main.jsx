224b6601649693174f3becf7d838d957b6243550
Mindaugas Purvis <mindaugas.purvis@gmail.com>
2023-07-09 10:43:08 +0300

    added isLoading

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PostCard from "../../components/PostCard/PostCard";
 import RegisterLayout from "../../layout/RegisterLayout/RegisterLayout";
 import axios from "axios";
@@ -6,37 +6,49 @@ import { useNavigate } from "react-router-dom";
 
 const Main = () => {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const navigate = useNavigate();
 
-  axios
-    .get("http://localhost:3000/posts")
-    .then((resp) => resp.data)
-    .then((response) => {
-      setPosts(response);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get("http://localhost:3000/posts")
+      .then((resp) => resp.data)
+      .then((response) => {
+        setPosts(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
   const onclickHandler = (id) => navigate(`/posts/${id}`);
 
   return (
     <RegisterLayout>
-      {posts.map((post, index) => (
-        <PostCard
-          key={index}
-          onClick={() => onclickHandler(post._id)}
-          nickname={post.nickname}
-          dateCreated={post.dateCreated}
-          title={post.title}
-          text={post.text}
-          commentNumber={post.comments.length}
-          edited={post.edited}
-        />
-      ))}
+      {isLoading ? (
+        <div>Loading</div>
+      ) : (
+        posts.map((post, index) => (
+          <PostCard
+            key={index}
+            onClick={() => onclickHandler(post._id)}
+            nickname={post.nickname}
+            dateCreated={post.dateCreated}
+            title={post.title}
+            text={post.text}
+            commentNumber={post.comments.length}
+            edited={post.edited}
+            postId={post._id}
+            userId={post.usersId}
+          />
+        ))
+      )}
     </RegisterLayout>
   );
 };
-
 export default Main;
